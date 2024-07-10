ba3d7333370fa7b5b3a93c6a3ced17c099f95f7e
huy23042001 <hataihuy2001@gmail.com>
2024-07-10 18:38:02 +0700

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: false,
-    apiUrl: 'http://blissful-reflection-production.up.railway.app:8080',
+    apiUrl: 'https://blissful-reflection-production.up.railway.app:8080',
 };
 
 /*
